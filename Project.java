55c270d6354fbbef0581992953f6b78e81eb158f
cgenglish <connor.g.english@gmail.com>
2024-03-22 11:29:07 -0700

    feat: add MergeSort and InsertionSort

--- Project.java
+++ Project.java
@@ -5,20 +5,30 @@
 public class Project {
     public static void main(String[] args) {
         int[] arr = { 3, 4, 21, 1, 58, 87, 89, 9000, 2, 1 };
+        int[] smallArr = { 3, 2, 5 };
 
         //System.out.println(arr); This gives us a destination value in memory
 
+        long startTime = System.nanoTime();
         for(int value : arr) {
             System.out.print(" " + value);
         }
 
 
-        SelectionSort(arr);
-        //BubbleSort(arr);
+        
+        // BubbleSort(arr);
+        // SelectionSort(arr);
+        // InsertionSort(arr);
+        
         System.out.println();
+
         for(int value : arr) {
             System.out.print(" " + value);
         }
+
+        long elapsedNanos = System.nanoTime() - startTime;
+        System.out.println();
+        System.out.println(elapsedNanos / 1000000);
     }
 
     public static void BubbleSort(int[] arr) {
@@ -55,7 +65,79 @@ public class Project {
             arr[minIndex] = temp;
 
         }
+    }
 
+    public static void InsertionSort(int[] arr) {
+        int temp, j;
+        //don't need to look at first element so can set i to 1 to be more efficient
+        for(int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+
+            for(j = i; j > 0 && arr[j-1] > temp; j--) {
+                 // this look will move over our elements to the right
+                 arr[j] = arr[j-1];
+            }
+            arr[j] = temp;
+        }
+    }
+
+    public static void MergeSort(int[] arr) {
+        int length = arr.length;
+
+        if(length <= 1) return;
+
+        int middle = length / 2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+
+        int l = 0; //index to keep track of left array
+        int r = 0; //index to keep track of right array
+
+        //populate the left and right arrays
+        for(; l < length; l++) {
+            if(l < middle){
+                leftArray[l] = arr[l];
+            } else{
+                rightArray[r] = arr[r];
+                r++;
+            }
+        }
+
+        MergeSort(leftArray);
+        MergeSort(rightArray);
+        Merge(leftArray, rightArray, arr);
+    }
+
+    public static void Merge(int[] leftArray, int[] rightArray, int[] array) {
+        int leftArraySize = leftArray.length;
+        int rightArraySize = rightArray.length;
+
+        int mainIndex = 0, leftArrIndex = 0, rightArrIndex = 0;
+        
+        //main condition for merge comparisons
+        if(leftArrIndex < leftArraySize && rightArrIndex < rightArraySize) {
+            if(leftArray[leftArrIndex] < rightArray[rightArrIndex]) {
+                array[mainIndex] = leftArray[leftArrIndex];
+                mainIndex++;
+                leftArrIndex++;
+            } else {
+                array[mainIndex] = rightArray[rightArrIndex];
+                mainIndex++;
+                rightArrIndex++;
+            }
+        }
+
+        while(leftArrIndex < leftArraySize) {
+            array[mainIndex] = leftArray[leftArrIndex];
+            mainIndex++;
+            leftArrIndex++;
+        }
+
+        while(rightArrIndex < rightArraySize) {
+            array[mainIndex] = rightArray[rightArrIndex];
+            mainIndex++;
+            rightArrIndex++;
+        }
 
     }
 }
